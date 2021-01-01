52602fb1382f2272339fc16c8d8843d32ba33d20
kazz2001 <kazz@planetky.com>
2021-01-01 20:15:39 +0900

    01012015 added best 2020

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -16,43 +16,46 @@ const FirstRightText = () => (
         <br/>R&amp;BやHip-HopなどBlack MusicのAlbum Reviewを載せています。1998年より、年50年枚程度レビューしています。
         <div>
 	      <Button href="/cd/2020/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
-            2020
-          </Button>
-          <Button href="/cd/2019/" kind="secondary" size="small" renderIcon={ArrowUpRight24}>
-            2019
-          </Button>
-          <Button href="/cd/2018/" kind="tertiary" size="small" renderIcon={ArrowUpRight24}>
-            2018
-          </Button>
-          <Button href="/cd/2017/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
-            2017
-          </Button>
-          <Button href="/cd/2016/" kind="secondary" size="small" renderIcon={ArrowUpRight24}>
-            2016
-          </Button>
+          2020
+        </Button>
+        <Button href="/cd/2019/" kind="secondary" size="small" renderIcon={ArrowUpRight24}>
+          2019
+        </Button>
+        <Button href="/cd/2018/" kind="tertiary" size="small" renderIcon={ArrowUpRight24}>
+          2018
+        </Button>
+        <Button href="/cd/2017/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
+          2017
+        </Button>
+        <Button href="/cd/2016/" kind="secondary" size="small" renderIcon={ArrowUpRight24}>
+          2016
+        </Button>
 	    </div>
         <br/><code>Black Music album Best 50</code>
         <br/>同じく、Black Musicのアルバム年間ベスト50です。順位は当サイトのオリジナルです。様々な音楽サイトを参考にしつつ、決めてます。
         <div>
-	      <Button href="/best50/2019/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
-            2019
-          </Button>
-          <Button href="/best50/2018/" kind="secondary" size="small" renderIcon={ArrowUpRight24}>
-            2018
-          </Button>
-          <Button href="/best50/2017/" kind="tertiary" size="small" renderIcon={ArrowUpRight24}>
-            2017
-          </Button>
-          <Button href="/best50/2016/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
-            2016
-          </Button>
+        <Button href="/best50/2020/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
+          2020
+        </Button>
+	      <Button href="/best50/2019/" kind="secondary" size="small" renderIcon={ArrowUpRight24}>
+          2019
+        </Button>
+        <Button href="/best50/2018/" kind="tertiary" size="small" renderIcon={ArrowUpRight24}>
+          2018
+        </Button>
+        <Button href="/best50/2017/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
+          2017
+        </Button>
+        <Button href="/best50/2016/" kind="secondary" size="small" renderIcon={ArrowUpRight24}>
+          2016
+        </Button>
 	    </div>
         <br/><code>Book Review</code>
         <br/>R&amp;BやHip-Hopをテーマにした書籍のレビューです。
         <div>
 	      <Button href="/book/bookreview1/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
-            Book Review
-          </Button>
+          Book Review
+        </Button>
 	    </div>
     </div>
 );
