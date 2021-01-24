eb707073b59b5305305a3ffe53475fc2d1de1527
kazz2001 <kazz@planetky.com>
2021-01-24 19:44:31 +0900

    10240945 added review dv

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -3,7 +3,7 @@ import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
 import Carbon from '../../images/carbon.jpg';
 import { Button } from 'carbon-components-react';
-import { ArrowUpRight24  } from '@carbon/icons-react';
+import { ArrowUpRight24, Search24 } from '@carbon/icons-react';
 // import { Launch } from '@carbon/pictograms-react';
 // import { Link } from "gatsby"
 
@@ -15,6 +15,7 @@ const FirstRightText = () => (
     <p>
       <code>Black Music Album Review</code>
       <br/>R&amp;BやHip-HopなどBlack MusicのAlbum Reviewを載せています。1998年より、年50年枚程度レビューしています。
+      <br/>下のボタンをクリックで年ごとのレビュー一覧へ。右上の<Search24 />をクリックで検索できます。
       <div style={{marginTop: '0.5rem',}}>
         <Button className="button-right-mergin" href="/cd/2020/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
           2020
@@ -34,6 +35,7 @@ const FirstRightText = () => (
 	    </div>
       <br/><code>Black Music Best 50 albums</code>
       <br/>同じく、Black Musicのアルバム年間ベスト50です。順位は当サイトのオリジナルです。様々な音楽サイトを参考にしつつ、決めてます。
+      <br/>下のボタンをクリックで年ごとのBest50へ。
       <div style={{marginTop: '0.5rem',}}>
         <Button className="button-right-mergin" href="/best50/2020/" kind="primary" size="small" renderIcon={ArrowUpRight24}>
           2020
