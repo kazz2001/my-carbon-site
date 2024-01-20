9b2853754827595be240b8cdd26ae0f6fccbda4f
kazz2001 <kazz@planetky.com>
2024-01-20 21:18:30 +0900

    202101202120

--- src/pages/review/adv3.js
+++ src/pages/review/adv3.js
@@ -3,18 +3,11 @@ import React from "react";
 const AdvJS3 = (props) => {
   return (
     <p>
-      <iframe
-        src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=288&l=ur1&category=musicunlimited&banner=1YSWBECW94PX2DF3BY82&f=ifr&linkID=21091d125badc7f55ae963341de03f20&t=planetkycom-22&tracking_id=planetkycom-22"
-        width="320"
-        height="50"
-        scrolling="no"
-        border="0"
-        marginwidth="0"
-        frameborder="0"
-        title="link to amazon"
-      ></iframe>
+      <br/><br/>
+      <a href="//ck.jp.ap.valuecommerce.com/servlet/referral?sid=2249009&pid=890147020" rel="nofollow">
+        <img src="//ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=2249009&pid=890147020" border="0" />
+      </a>
     </p>
   );
 };
 export default AdvJS3;
-
